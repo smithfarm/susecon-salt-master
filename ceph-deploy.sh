67ec696a9a8f7bd61c8e76d928d2582c6213d97a
Nathan Cutler <ncutler@suse.com>
2015-10-29 10:34:29 +0100

    ceph-deploy-sh.sls: Salt State for running ceph-deploy.sh
    Signed-off-by: Nathan Cutler <ncutler@suse.com>

--- ceph-deploy.sh
+++ ceph-deploy.sh
@@ -5,6 +5,7 @@
 # Deploy a Ceph cluster on a delegate's mon/osd nodes
 # (to be run as user 'ceph' from the admin node)
 #
+source .bashrc
 ceph-deploy new $node1 $node2 $node3 $node4
 ceph-deploy mon create-initial
 ceph-deploy osd prepare $node1:xvdb
