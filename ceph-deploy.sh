9316315065327933f2dabcb7118197ad1a50bd6b
Nathan Cutler <ncutler@suse.com>
2016-10-17 13:36:18 +0200

    ceph-deploy.sh: set admin keyring permissions to 644
    Signed-off-by: Nathan Cutler <ncutler@suse.com>

--- ceph-deploy.sh
+++ ceph-deploy.sh
@@ -13,7 +13,12 @@ ceph-deploy osd prepare $node2:xvdb
 ceph-deploy osd prepare $node3:xvdb
 ceph-deploy osd prepare $node4:xvdb
 ceph-deploy admin $ADMIN
+ssh $ADMIN sudo chmod 644 /etc/ceph/ceph.client.admin.keyring
 ceph-deploy admin $node1
+ssh $node1 sudo chmod 644 /etc/ceph/ceph.client.admin.keyring
 ceph-deploy admin $node2
+ssh $node2 sudo chmod 644 /etc/ceph/ceph.client.admin.keyring
 ceph-deploy admin $node3
+ssh $node3 sudo chmod 644 /etc/ceph/ceph.client.admin.keyring
 ceph-deploy admin $node4
+ssh $node4 sudo chmod 644 /etc/ceph/ceph.client.admin.keyring
