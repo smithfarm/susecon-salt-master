6d404ef27dd01a4f79deed4f5465cb4ec774af73
Nathan Cutler <ncutler@suse.com>
2016-10-18 15:48:26 +0200

    resiliency-data.sh: reduce # of objs from 1000 to 400
    Signed-off-by: Nathan Cutler <ncutler@suse.com>

--- resiliency-data.sh
+++ resiliency-data.sh
@@ -5,7 +5,7 @@
 # Prime cluster with data (for Resiliency lab)
 #
 dd if=/dev/zero of=/tmp/foo bs=4K count=1K # 4M file
-for i in {1..1000}
+for i in {1..400}
 do  
     rados -p rbd put lb$i /tmp/foo
 done
