818e2decde455a8105aca7c2f2dd484480c3aba5
Nathan Cutler <ncutler@suse.com>
2017-09-13 21:04:50 +0200

    deepsea-install.sh: fix script so it actually works
    Signed-off-by: Nathan Cutler <ncutler@suse.com>

--- deepsea-install.sh
+++ deepsea-install.sh
@@ -1,7 +1,10 @@
-#!/bin/sh
-zypper --non-interactive install --no-recommends make
-git clone --depth 1 https://github.com/SUSE/DeepSea.git
+#!/bin/bash -x
+#
+# install DeepSea on local master (will be run as root)
+#
+zypper --non-interactive install --no-recommends make rpm
+test -d DeepSea || git clone --depth 1 https://github.com/SUSE/DeepSea.git
 cd DeepSea
+ls -l
 make install
-zypper --non-interactive install --no-recommends \
-    $(rpmspec --requires -q -v DeepSea/deepsea.spec | grep manual | awk \'{print $2}\')
+zypper --non-interactive install --no-recommends $(rpmspec --requires -q -v deepsea.spec | grep manual | awk '{print $2}')
