de6277caac8946e88ae00776b8cf618dc9918faf
Nathan Cutler <ncutler@suse.com>
2017-09-13 22:44:41 +0200

    deepsea-install.sh: pre-install python-click from OBS
    Signed-off-by: Nathan Cutler <ncutler@suse.com>

--- deepsea-install.sh
+++ deepsea-install.sh
@@ -2,7 +2,19 @@
 #
 # install DeepSea on local master (will be run as root)
 #
-sudo zypper --non-interactive install --no-recommends make rpm
+
+#---------------------------------------------------------
+#--- can be dropped once python-click reaches SES5 channel
+#---------------------------------------------------------
+sudo zypper --non-interactive --no-gpg-checks addrepo http://download.opensuse.org/repositories/devel:/languages:/python/SLE_12_SP3/devel:languages:python.repo
+sudo zypper --non-interactive --no-gpg-checks refresh
+sudo zypper --non-interactive --no-gpg-checks install --no-recommends python-click
+sudo zypper --non-interactive --no-gpg-checks removerepo devel_languages_python
+#---------------------------------------------------------
+#--- can be dropped once python-click reaches SES5 channel
+#---------------------------------------------------------
+
+sudo zypper --non-interactive --no-gpg-checks install --no-recommends make rpm
 test -d DeepSea || git clone --depth 1 --branch susecon2017 https://github.com/smithfarm/DeepSea.git
 cd DeepSea
 ls -l
