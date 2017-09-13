9d3eb796818d27625c4a34e64bf3253d7aedda2e
Nathan Cutler <ncutler@suse.com>
2017-09-13 21:27:34 +0200

    deepsea-install.sh: clone susecon2017 branch from smithfarm/DeepSea.git
    Signed-off-by: Nathan Cutler <ncutler@suse.com>

--- deepsea-install.sh
+++ deepsea-install.sh
@@ -3,7 +3,7 @@
 # install DeepSea on local master (will be run as root)
 #
 zypper --non-interactive install --no-recommends make rpm
-test -d DeepSea || git clone --depth 1 https://github.com/SUSE/DeepSea.git
+test -d DeepSea || git clone --depth 1 --branch susecon2017 https://github.com/smithfarm/DeepSea.git
 cd DeepSea
 ls -l
 make install
